c7c0d174f0d98df19c1c8aea26292c8d9058ba66
rajanikantyellure <rajanikantyellure86@gmail.com>
2021-12-05 16:47:46 +0530

    UMS-2:create spring boot application

---
+++ src/main/java/com/bikkadIt/UserManangementySystemApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManangementySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManangementySystemApplication.class, args);
+	}
+
+}
